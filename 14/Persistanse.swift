ed3f14c231f4cb01d443eecea629b283492c088f
Dmitro Vinokurov <vinokurov1998@icloud.com>
2020-02-16 12:43:25 +0500

    first commit

--- 14/Persistanse.swift
+++ 14/Persistanse.swift
@@ -1,9 +1,39 @@
-//
-//  Persistanse.swift
-//  14
-//
-//  Created by Дмитро Винокуров on 16.02.2020.
-//  Copyright © 2020 Дмитрий Винокуров. All rights reserved.
-//
-
 import Foundation
+import RealmSwift
+
+class Dog: Object{
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
+
+class Persistance{
+    static let shared = Persistance()
+    
+    private let realm = try! Realm()
+    
+    func test(){
+//        try! realm.write {
+//            let dog = Dog()
+//            dog.name = "Пчёла"
+//            realm.add(dog)
+//
+//            let dog1 = Dog()
+//            dog1.name = "Космос"
+//            realm.add(dog1)
+//
+//            let dog2 = Dog()
+//            dog2.name = "Саня белый"
+//            realm.add(dog2)
+//        }
+
+//        let allDogs = realm.objects(Dog.self)
+//        try! realm.write {
+//            for (index, dog) in allDogs.enumerated(){
+//                dog.age = index * 2 + 2
+//        }
+//        }
+        for dog in realm.objects(Dog.self){
+            print("--\(dog.name): \(dog.age)")
+        }
+    }
+}
--- 14/ViewController.swift
+++ 14/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  14
-//
-//  Created by Дмитро Винокуров on 16.02.2020.
-//  Copyright © 2020 Дмитрий Винокуров. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Persistance.shared.test()
+//        Persistance.shared.userName = "Dmitro"
+//        print(Persistance.shared.userName)
     }
 
 
