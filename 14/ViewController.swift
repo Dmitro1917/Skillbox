fa76b6f7ef5d96740d7aaf93a982400b7064a8cb
Dmitro Vinokurov <vinokurov1998@icloud.com>
2020-02-16 09:48:25 +0500

    Initial Commit

---
+++ 14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  14
+//
+//  Created by Дмитро Винокуров on 16.02.2020.
+//  Copyright © 2020 Дмитрий Винокуров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
